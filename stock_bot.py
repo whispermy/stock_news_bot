0b2219cff32911bcc9337618076374e7ee11fd35
whispermy <49235958+whispermy@users.noreply.github.com>
2021-04-15 06:20:27 +0900

    ver0.1 기본 기능 완료

--- stock_bot.py
+++ stock_bot.py
@@ -17,6 +17,8 @@ import os
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 import json
+
+import locale
 #-----------definition and basic setting-----------
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
@@ -25,16 +27,18 @@ options.add_argument('--ignore-ssl-errors')
 driver = webdriver.Chrome(options=options)
 
 # send slack message function
-def post_message(token, dict_val):
+def post_message(token, channel, text):
     response = requests.post("https://slack.com/api/chat.postMessage",
         headers={"Authorization": "Bearer "+token},
-        #data={"channel": channel,"text": text}
-        data=json.dumps(dict_val)
+        data={"channel": channel,"text": text}
     )
     print(response)
 
+# setting locale to use Korean
+locale.setlocale(locale.LC_ALL, '')
+
 # setting slack token
-myToken = "xoxb-1949658563270-1953364834789-44sDT2LqthWpa8gD6qHquIUH"
+myToken = "slack-bot-token"
 
 # setting url
 url = "https://www.hankyung.com/"
@@ -56,8 +60,11 @@ print("DEBUG Message: All popup closed.\n")
 
 # page initializing
 driver.switch_to_window(main[0])
-slack_msg = {'channel': '#general', 'text': ''}
-# news_list = []
+now = datetime.now()
+news_list = []
+
+today = now.date().strftime('%Y년 %m월 %d일 %A')
+news_list.append(today+' 경제 뉴스 모음\n\n\n')
 # page_num = 101
 
 for page_num in range(102,112):
@@ -68,14 +75,12 @@ for page_num in range(102,112):
 
     # save the section name
     section_name = driver.find_element_by_xpath("/html/body/div[1]/div/div[3]/div[1]/div[1]/div[1]/h2").text
-    #news_list.append(section_name)
-    slack_msg['text'].append(section_name)
-
+    news_list.append('\n\n---------------- '+section_name+' ------------------')
+    
     # set article row
     selectArticle_ul = 1
     selectArticle_li = 1
     subpage = 2
-    now = datetime.now()
     day_diff = 0
 
     while(True):
@@ -94,12 +99,10 @@ for page_num in range(102,112):
 
             # 제목 불러오기 및 스트링 저장, 배열 저장 (슬랙 메세지 만들기)
             news_title = driver.find_element_by_xpath("/html/body/div[1]/div/div[3]/div[1]/div[1]/ul["+str(selectArticle_ul)+"]/li["+str(selectArticle_li)+"]/div[1]/h3/a").text
-            #news_list.append(news_title)
-            slack_msg['text'].append(news_title)
+            news_list.append(news_title)
             print("DEBUG MESSAGE: 기사 제목:'",news_title,"' list added.")
             news_url = driver.find_element_by_xpath("/html/body/div[1]/div/div[3]/div[1]/div[1]/ul["+str(selectArticle_ul)+"]/li["+str(selectArticle_li)+"]/div[1]/h3/a").get_attribute("href")
-            #news_list.append(news_url)
-            slack_msg['text'].append(news_url)
+            news_list.append(news_url)
             print("DEBUG MESSAGE: 기사 URL:'",news_url,"' list added.")
 
         if selectArticle_ul >= 5:
@@ -123,11 +126,20 @@ for page_num in range(102,112):
         
 time.sleep(1)
 driver.close()
-print("DEBUG MESSAGE: closed")
-print(news_list)
-# repeat all the tabs
+news_list.append('\n\n---------------- 뉴스 끝 ------------------')
+print("DEBUG MESSAGE: page closed.")
+# print(news_list)
 
-# send slack bot example
-post_message(myToken,slack_msg)
+# change list to str
+slack_msg = ''
+for n in news_list:
+    slack_msg = slack_msg+'\n'+n
+print(slack_msg)
+
+# send slack bot all messages
+post_message(myToken,'#general',slack_msg)
+print("DEBUG MESSAGE: send slack message:",slack_msg)
+
+print("DEBUG MESSAGE: program closed.")
 
 
--- test.py
+++ test.py
@@ -24,25 +24,59 @@
 
 import requests
 import json
- 
+from datetime import datetime
+import time
+import locale 
+
 def post_message(token, channel, text):
     response = requests.post("https://slack.com/api/chat.postMessage",
         headers={"Authorization": "Bearer "+token},
         data={"channel": channel,"text": text}
-        #data=json.dumps(dict_val)
     )
+    # data={"channel": channel,"text": text}
+    # print(data)
     print(response)
  
-myToken = "xoxb-1949658563270-1953364834789-44sDT2LqthWpa8gD6qHquIUH"
-dic = {'channel': '#general', 'text': 'jocoding'}
-#dic['text'].append(0000)
+# def post_message2(token, diction):
+#     response = requests.post("https://slack.com/api/chat.postMessage",
+#         headers={"Authorization": "Bearer "+token},
+#         data=diction
+#     )
+#     # print("dictionary")
+#     # print(json.dumps(diction))
+#     print(response)
+ 
 
-print(dic)
+myToken = "slack-bot-token"
+# dic = {'channel': '#general', 'text': ['hello\nhelllll\nhi']}
+# dic['text'].append(0000)
 
+# print(dic)
 
-# post_message(myToken,dic)
-post_message(myToken,'#general',"textetetet")
+print(locale.getlocale())
+locale.setlocale(locale.LC_ALL, '')
+print(locale.getlocale())
 
+today = datetime.now().date()
+print(today)
+print(today.strftime('%Y. %m. %d.'))
+print(today.strftime('%Y년 %m월 %d일 %A'))
 
+# print(today.strftime('%Y년 %m월 %d일 %A'.encode('unicode-escape').decode()
+#     ).encode().decode('unicode-escape')
+# )
+
+# news_list = ['news1','nes3','1235g','1231241rf','dsfasdasdgasdg']
+# val = '123124'
+# # news_list.append('asdf'+now)
+# slack_msg = ''
+
+# for n in news_list:
+#     slack_msg = slack_msg+'\n'+n
+# print(slack_msg)
+
+# post_message(myToken,'#general',slack_msg)
+
+# post_message2(myToken,dic)
 
 
